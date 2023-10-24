bf0e85631a89bd972781cfedb462a7a9a7bf3186
Cillian <G00404411@atu.ie>
2023-10-24 16:22:26 +0100

    Finished part 1

--- src/main/java/ie/atu/cicd_week5/CicdWeek5Application.java
+++ src/main/java/ie/atu/cicd_week5/CicdWeek5Application.java
@@ -2,12 +2,21 @@ package ie.atu.cicd_week5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CicdWeek5Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdWeek5Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
